1bad3d189409cfb69080898c2e41d71f3057cda9
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2020-05-13 13:45:00 -0300

    Second solution. Store file in memory

--- src/main.c
+++ src/main.c
@@ -1,24 +1,33 @@
-/* ftruncate ftell */
-#define _POSIX_C_SOURCE 200112L
-
-/* fileno */
 #include <stdio.h>      // file wrangling
 #include <arpa/inet.h>  // hton family
 
-/* ftruncate */
-#include <unistd.h>
+// fstat
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define DATAFILE "nros2bytes.dat"
 
 int main(int argc, char **argv) {
-  FILE *r_handle = fopen("nros2bytes.dat", "r");
-  FILE *w_handle = fopen("nros2bytes.dat", "r+");
+  // read the entire thing to memory
+  struct stat stats_buf;
+  stat(DATAFILE, &stats_buf);
 
-  int x;
-  while (fread(&x, 2, 1, r_handle)) {
-    int x_int = ntohs(x);
-    if (x_int % 3)
-      continue;
-    fwrite(&x, 2, 1, w_handle);
+  const int n = stats_buf.st_size / 2;
+  int file_buf[n];
+
+  FILE *file = fopen(DATAFILE, "r");
+  fread(file_buf, 2, n, file);
+  fclose(file);
+
+  // write with duplication
+  file = fopen(DATAFILE, "w");
+  int *ptr = file_buf;
+  int *end = file_buf + n;
+  for (; ptr != end; ptr++) {
+    int x = ntohs(*ptr);
+    fwrite(ptr, 2, 1, file);
+    if (!(x % 3))
+      fwrite(ptr, 2, 1, file);
   }
-  ftruncate(fileno(w_handle), ftell(w_handle));
 }

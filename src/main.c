2f7d2101f5b0fbea5c102bde2a21f94200a2fc08
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2020-05-18 13:32:06 -0300

    refactor: abstracted low level file twiddling

--- src/main.c
+++ src/main.c
@@ -8,6 +8,23 @@
 
 #define DATAFILE "nros2bytes.dat"
 
+static inline int is_divisible(uint16_t x) {
+  x = ntohs(x);
+  return x % 3 == 0;
+}
+
+static inline uint16_t read_still(FILE *f) {
+  uint16_t x;
+  fread(&x, sizeof(x), 1, f);
+  fseek(f, -sizeof(x), SEEK_CUR);
+  return x;
+}
+
+static inline void write_still(FILE *f, uint16_t x) {
+  fwrite(&x, sizeof(x), 1, f);
+  fseek(f, -sizeof(x), SEEK_CUR);
+}
+
 int main(int argc, char **argv) {
   // keep a write pointer to the end of the file
   FILE *w_handle = fopen(DATAFILE, "r+");
@@ -16,9 +33,8 @@ int main(int argc, char **argv) {
   // keep track of final file size
   FILE *r_handle = fopen(DATAFILE, "r");
   for (uint16_t x; fread(&x, sizeof(x), 1, r_handle); ) {
-    uint16_t dx = ntohs(x);         // "decoded x", because of ~ENDIANNESS~
     // duplicated items take up extra space
-    if (!(dx % 3))
+    if (is_divisible(x))
       fseek(w_handle, sizeof(x), SEEK_CUR);
   }
 
@@ -27,22 +43,13 @@ int main(int argc, char **argv) {
   while (ftell(r_handle) != ftell(w_handle)) {
     uint16_t x;
     fseek(r_handle, -sizeof(x), SEEK_CUR);
+    x = read_still(r_handle);
 
-    fread(&x, sizeof(x), 1, r_handle);
-    fseek(r_handle, -sizeof(x), SEEK_CUR);          // reading moves the file
-
-    uint16_t dx = ntohs(x);                         // "decoded x"
-    const int ncopies = dx % 3 ? 1 : 2;
+    const int ncopies = is_divisible(x) ? 2 : 1;
   
-    fseek(w_handle, -sizeof(x), SEEK_CUR);
-    fwrite(&x, 2, 1, w_handle);
-    if (ncopies == 2) {
-      // NOTE: writing two items with a single fwrite() does not result on two
-      //       copies of x. fwrite writes x, keeps going, and writes the next
-      //       thing on the stack (dx)
-      fseek(w_handle, -2*sizeof(x), SEEK_CUR);      // writing moves the file
-      fwrite(&x, 2, 1, w_handle);
+    for (int i = 0; i < ncopies; i++) {
+      fseek(w_handle, -sizeof(x), SEEK_CUR);
+      write_still(w_handle, x);
     }
-    fseek(w_handle, -sizeof(x), SEEK_CUR);          // writing moves the file
   }
 }

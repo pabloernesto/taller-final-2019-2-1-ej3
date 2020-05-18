1e603af212c155ebb2d3a5b7d92541fa653d66fc
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2020-05-18 13:16:58 -0300

    Fourth solution. Two passes

--- src/main.c
+++ src/main.c
@@ -8,41 +8,41 @@
 
 #define DATAFILE "nros2bytes.dat"
 
-// copy the character at pos into (pos + offset)
-// this operation does not preserve the current file position
-static void scoot_byte(FILE *file, size_t pos, size_t offset) {
-  fseek(file, pos, SEEK_SET);
-  char c = fgetc(file);
-  offset--;                       // fgetc advances the file by one position
-  fseek(file, offset, SEEK_CUR);
-  fputc(c, file);
-}
-
-// copy [pos, pos+n) to [pos+offset, pos+n+offset), using constant memory
-// this operation does not preserve the current file position
-void scoot(FILE *file, size_t n, size_t pos, size_t offset) {
-  while (n > 0) {
-    n--;
-    scoot_byte(file, pos+n, offset);
-  }
-}
-
 int main(int argc, char **argv) {
-  struct stat stats_buf;
-  stat(DATAFILE, &stats_buf);
-  int n = stats_buf.st_size / 2;    // how many unprocessed shorts there are
+  // keep a write pointer to the end of the file
+  FILE *w_handle = fopen(DATAFILE, "r+");
+  fseek(w_handle, 0, SEEK_END);
 
-  FILE *file = fopen(DATAFILE, "r+");
-  for (; n; n--) {
+  // keep track of final file size
+  FILE *r_handle = fopen(DATAFILE, "r");
+  for (uint16_t x; fread(&x, sizeof(x), 1, r_handle); ) {
+    uint16_t dx = ntohs(x);         // "decoded x", because of ~ENDIANNESS~
+    // duplicated items take up extra space
+    if (!(dx % 3))
+      fseek(w_handle, sizeof(x), SEEK_CUR);
+  }
+
+  // copy into final positions
+  // r_handle just finished reading all items, so it at EOF
+  while (ftell(r_handle) != ftell(w_handle)) {
     uint16_t x;
-    fread(&x, sizeof(x), 1, file);
-    uint16_t dx = ntohs(x);         // standing for "decoded x"
-    if (!(dx % 3)) {
-      size_t pos = ftell(file);
-      size_t bytes_left = (n-1) * sizeof(x);
-      scoot(file, bytes_left, pos, sizeof(x));
-      fseek(file, pos, SEEK_SET);
-      fwrite(&x, 2, 1, file);
+    fseek(r_handle, -sizeof(x), SEEK_CUR);
+
+    fread(&x, sizeof(x), 1, r_handle);
+    fseek(r_handle, -sizeof(x), SEEK_CUR);          // reading moves the file
+
+    uint16_t dx = ntohs(x);                         // "decoded x"
+    const int ncopies = dx % 3 ? 1 : 2;
+  
+    fseek(w_handle, -sizeof(x), SEEK_CUR);
+    fwrite(&x, 2, 1, w_handle);
+    if (ncopies == 2) {
+      // NOTE: writing two items with a single fwrite() does not result on two
+      //       copies of x. fwrite writes x, keeps going, and writes the next
+      //       thing on the stack (dx)
+      fseek(w_handle, -2*sizeof(x), SEEK_CUR);      // writing moves the file
+      fwrite(&x, 2, 1, w_handle);
     }
+    fseek(w_handle, -sizeof(x), SEEK_CUR);          // writing moves the file
   }
 }
